77350a43d7384dccfcc1cd07004070c2bd69ae36
User <you@example.com>
2026-05-05 04:44:40 +0500

    Create star schema tables for dw

--- 2_DW_Mart_Build/01_create_tables_dw.sql
+++ 2_DW_Mart_Build/01_create_tables_dw.sql
@@ -1,1 +1,50 @@
 -- Step 1: DW - Create star schema tables
+
+DROP TABLE IF EXISTS skills_job_dim;
+DROP TABLE IF EXISTS job_postings_fact;
+DROP TABLE IF EXISTS company_dim;
+DROP TABLE IF EXISTS skills_dim;
+
+CREATE Table company_dim(
+    company_id INTEGER PRIMARY KEY,
+    name VARCHAR
+);
+
+CREATE TABLE skills_dim(
+    skill_id INTEGER PRIMARY KEY,
+    skill VARCHAR,
+    TYPE VARCHAR
+);
+
+create Table job_postings_fact(
+    job_id INTEGER PRIMARY KEY,
+    company_id INTEGER,
+    job_title_short VARCHAR,
+    job_title VARCHAR,
+    job_location VARCHAR,
+    job_via VARCHAR,
+    job_schedule_type VARCHAR,
+    job_work_from_home BOOLEAN,
+    search_location VARCHAR,
+    job_posted_date TIMESTAMP,
+    job_no_degree_mention BOOLEAN,
+    job_health_insurance BOOLEAN,
+    job_country VARCHAR,
+    salary_rate VARCHAR,
+    salary_year_avg DOUBLE,
+    salary_hour_avg DOUBLE,
+    FOREIGN KEY (company_id) REFERENCES company_dim(company_id)
+);
+
+CREATE Table skills_job_dim(
+    skill_id INTEGER,
+    job_id INTEGER,
+    PRIMARY KEY(skill_id, job_id),
+    Foreign Key (skill_id) REFERENCES skills_dim(skill_id),
+    Foreign Key (job_id) REFERENCES job_postings_fact(job_id)
+);
+
+
+SELECT table_name
+from information_schema.tables
+WHERE table_schema = 'main';
