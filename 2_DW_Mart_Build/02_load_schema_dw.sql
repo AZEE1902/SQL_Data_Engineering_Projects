2aab41144186ce78aca9978192388161a77450a6
User <you@example.com>
2026-05-05 06:05:01 +0500

    data validation check sat

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -60,3 +60,21 @@ SELECT '=== Loading skills_job_dim Table ===' AS info;
 INSERT INTO skills_job_dim(skill_id, job_id)
 select skill_id, job_id
 from read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv', auto_detect=true);
+
+SELECT '=== Row Counts ===' AS info;
+select 'Company Dim' as table_name, count(*) as record_count from company_dim
+UNION ALL
+select 'Skills Dim' as table_name, count(*) from skills_dim
+UNION ALL
+select 'Job Postings Fact' as table_name, count(*)from job_postings_fact
+UNION ALL
+select 'Skills Job Dim' as table_name, count(*)from skills_job_dim;
+
+SELECT '=== Company Dimension Sample ===' as info;
+select * from company_dim LIMIT 5;
+SELECT '=== Skills Dimension Sample ===' as info;
+select * from skills_dim LIMIT 5;
+SELECT '=== Job Posting Fact Sample ===' as info;
+select * from job_postings_fact LIMIT 5;
+SELECT '=== Skills job bridge Sample ===' as info;
+select * from skills_job_dim LIMIT 5;
--- 2_DW_Mart_Build/build_marts.sql
+++ 2_DW_Mart_Build/build_marts.sql
@@ -1,6 +1,9 @@
--- Step 1 : DW - Create star schema tables
+--  duckdb dw_marts.duckdb -c ".read build_marts.sql"
+
+
+-- Step 1: DW - Create star schema tables
 .read 01_create_tables_dw.sql
 
--- Step 2: DW - Load data from CSV files into table
 
-.read 02_load_schema_dw.sql  
+-- Setp 2: DW - Load data from CSV files into tables
+.read 02_load_schema_dw.sql
