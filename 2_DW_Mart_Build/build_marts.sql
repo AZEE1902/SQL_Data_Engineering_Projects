06d2464c70574d43acd7ff7b720d738b58b477cd
Syed Muhammad Askari Zaidi <120264394+AZEE1902@users.noreply.github.com>
2026-05-06 06:35:53 +0500

    Add steps to create and update Priority mart

--- 2_DW_Mart_Build/build_marts.sql
+++ 2_DW_Mart_Build/build_marts.sql
@@ -13,3 +13,9 @@
 
 -- Step 4: Mart - Create skills demand mart
 .read 04_create_skills_mart.sql
+
+-- Step 5: Mart - Create Priority mart
+.read 05_create_priority_mart.sql
+
+-- step 6: Mart - Update Priority mart
+.read 06_update_priority_mart.sql
